b4b3ee984a1d9d055dd6b421ca2fa4410b5b2f52
cinemapub <cinemapub@users.noreply.github.com>
2020-11-17 09:03:12 +0000

    Fix styling

--- tests/AnalyzerTest.php
+++ tests/AnalyzerTest.php
@@ -7,21 +7,21 @@ use PHPUnit\Framework\TestCase;
 
 class AnalyzerTest extends TestCase
 {
-
     public function __construct(?string $name = null, array $data = [], $dataName = '')
     {
         parent::__construct($name, $data, $dataName);
     }
 
-    public function testAnalysis(){
-        $exampleFolder=__DIR__;
-        $analysis=(new Analyzer)->analyze("$exampleFolder/sources/example.gif");
-        $analysis=(new Analyzer)->analyze("$exampleFolder/sources/example.mp4");
-        $analysis=(new Analyzer)->analyze("$exampleFolder/sources/example.png");
-        $analysis=(new Analyzer)->analyze("$exampleFolder/sources/big_buck_bunny.m4a");
-        $analysis=(new Analyzer)->analyze("$exampleFolder/sources/big_buck_bunny.mp3");
-        $analysis=(new Analyzer)->analyze("$exampleFolder/sources/big_buck_bunny5.mp4");
-        $analysis=(new Analyzer)->analyze("$exampleFolder/sources/big_buck_bunny5.wavZZ");
-        $this->assertGreaterThan(0,$analysis,"fake");
+    public function testAnalysis()
+    {
+        $exampleFolder = __DIR__;
+        $analysis = (new Analyzer)->analyze("$exampleFolder/sources/example.gif");
+        $analysis = (new Analyzer)->analyze("$exampleFolder/sources/example.mp4");
+        $analysis = (new Analyzer)->analyze("$exampleFolder/sources/example.png");
+        $analysis = (new Analyzer)->analyze("$exampleFolder/sources/big_buck_bunny.m4a");
+        $analysis = (new Analyzer)->analyze("$exampleFolder/sources/big_buck_bunny.mp3");
+        $analysis = (new Analyzer)->analyze("$exampleFolder/sources/big_buck_bunny5.mp4");
+        $analysis = (new Analyzer)->analyze("$exampleFolder/sources/big_buck_bunny5.wavZZ");
+        $this->assertGreaterThan(0, $analysis, "fake");
     }
 }
--- tests/FfmpegTest.php
+++ tests/FfmpegTest.php
@@ -2,8 +2,8 @@
 
 namespace Brightfish\SpxMediaAnalyzer\Tests;
 
-use Brightfish\SpxMediaAnalyzer\Ffmpeg;
 use \Exception;
+use Brightfish\SpxMediaAnalyzer\Ffmpeg;
 use PHPUnit\Framework\TestCase;
 
 class FfmpegTest extends TestCase
@@ -17,9 +17,11 @@ class FfmpegTest extends TestCase
     public function __construct(?string $name = null, array $data = [], $dataName = '')
     {
         parent::__construct($name, $data, $dataName);
-        $this->ffmpeg=New ffmpeg();
-        $this->tempFolder=__DIR__ . "/" . "temp";
-        if(!is_dir($this->tempFolder))  mkdir($this->tempFolder);
+        $this->ffmpeg = new ffmpeg();
+        $this->tempFolder = __DIR__ . "/" . "temp";
+        if (! is_dir($this->tempFolder)) {
+            mkdir($this->tempFolder);
+        }
     }
 
     public function testUse_ffmpeg()
@@ -29,25 +31,26 @@ class FfmpegTest extends TestCase
         $this->ffmpeg->use_ffmpeg("/usr/bin/ffmpeg");
     }
 
-    public function testLog_to_folder(){
+    public function testLog_to_folder()
+    {
         $this->expectException(Exception::class);
         $this->ffmpeg->log_to_folder("/does/not/exist");
         $this->ffmpeg->log_to_folder(".");
     }
 
-    public function testRun(){
-        $exampleFolder=__DIR__;
+    public function testRun()
+    {
+        $exampleFolder = __DIR__;
         $this->ffmpeg->cache_to_folder($this->tempFolder);
         $this->ffmpeg->log_to_folder($this->tempFolder);
-        $output=$this->ffmpeg->run_ffmpeg("$exampleFolder/sources/example.mp4","-",[],true);
-        $this->assertGreaterThan(0,count($output),"lines");
+        $output = $this->ffmpeg->run_ffmpeg("$exampleFolder/sources/example.mp4", "-", [], true);
+        $this->assertGreaterThan(0, count($output), "lines");
     }
 
     public function __destruct()
     {
-        if(is_dir($this->tempFolder)){
+        if (is_dir($this->tempFolder)) {
             exec("rm -fr \"$this->tempFolder\" ");
         }
     }
-
 }
