03084902b28dabecea74693318c72c2c53aa108d
Peter Forret <peter@forret.com>
2021-02-23 10:00:18 +0100

    clean up DCP Probe

--- src/DcpProbe.php
+++ src/DcpProbe.php
@@ -19,7 +19,8 @@ class DcpProbe
         $cpl_body = $cpl_dom->children();
         $data = [];
 
-        $data["command"]["binary"] = $this->version;
+        $data["command"]["binary"] = "DcpProbe::probe";
+        $data["command"]["version"] = $this->version;
         $data["command"]["input"] = [
             "filename" => $input,
             "filesize" => $this->get_dcp_size($input),
@@ -29,35 +30,51 @@ class DcpProbe
         $data["command"]["started_at"] = date("c");
         $t0 = microtime(true);
 
-        $data["uuid"] = str_replace("urn:uuid:", "", (string)$cpl_body->Id);
-        $data["content_title"] = (string)$cpl_body->ContentTitleText;
-        $data["issue_date"] = Carbon::parse($cpl_body->IssueDate);
-        $data["annotation_text"] = (string)($cpl_body->AnnotationText);
-        $data["issuer"] = (string)($cpl_body->Issuer);
-        $data["creator"] = (string)($cpl_body->Creator);
-        $data["content_kind"] = (string)($cpl_body->ContentKind);
-        $data["rating_list"] = (string)($cpl_body->RatingList);
-        $data["total_reels"] = count($cpl_body->ReelList);
-        $data["total_frames"] = 0;
-        foreach ($cpl_body->ReelList->Reel as $reel) {
-            $data["reel_uuid"] = (string)($reel->Id);
-            if (! isset($reel->AssetList)) {
-                continue;
+        foreach($cpl_body->children() as $cpl_tag){
+            $tag_name=$cpl_tag->getName();
+            switch($tag_name){
+                case "Id":
+                    $data["uuid"] = str_replace("urn:uuid:", "", (string)$cpl_tag);
+                    break;
+
+                case "AnnotationText":
+                case "ContentKind":
+                case "ContentTitleText":
+                case "Creator":
+                case "Issuer":
+                case "RatingList":
+                    $data[$tag_name] = (string)$cpl_tag;
+                    break;
+
+                case "IssueDate":
+                    $data[$tag_name] = Carbon::parse($cpl_tag);
+                    break;
+
             }
-            foreach ($reel->AssetList as $asset) {
-                if (isset($asset->MainPicture)) {
-                    // video asset
-                    $data["aspect_ratio"] = (string)$asset->MainPicture->ScreenAspectRatio;
-                    $data["frame_rate"] = (string)$asset->MainPicture->FrameRate;
-                    $fps_parts = explode(' ', $data["frame_rate"]);
-                    if (count($fps_parts) == 2) {
-                        $data["fps"] = round($fps_parts[0] / $fps_parts[1], 3);
-                    } else {
-                        $data["fps"] = round($data["frame_rate"], 3);
+        }
+        if(isset($cpl_body->ReelList)){
+            $data["total_reels"] = count($cpl_body->ReelList);
+            $data["total_frames"] = 0;
+            foreach ($cpl_body->ReelList->Reel as $reel) {
+                $data["reel_uuid"] = (string)($reel->Id);
+                if (! isset($reel->AssetList)) {
+                    continue;
+                }
+                foreach ($reel->AssetList as $asset) {
+                    if (isset($asset->MainPicture)) {
+                        // video asset
+                        $data["aspect_ratio"] = (string)$asset->MainPicture->ScreenAspectRatio;
+                        $data["frame_rate"] = (string)$asset->MainPicture->FrameRate;
+                        $fps_parts = explode(' ', $data["frame_rate"]);
+                        if (count($fps_parts) == 2) {
+                            $data["fps"] = round($fps_parts[0] / $fps_parts[1], 3);
+                        } else {
+                            $data["fps"] = round($data["frame_rate"], 3);
+                        }
+                        $data["frames"] = (int)$asset->MainPicture->Duration;
+                        $data["total_frames"] += $data["frames"];
+                        $data["seconds"] = round($data["total_frames"] / $data["fps"], 4);
                     }
-                    $data["frames"] = (int)$asset->MainPicture->Duration;
-                    $data["total_frames"] += $data["frames"];
-                    $data["seconds"] = round($data["frames"] / $data["fps"], 4);
                 }
             }
         }
@@ -65,25 +82,6 @@ class DcpProbe
         $data["command"]["finished_at"] = date("c");
         $duration = round($t1 - $t0, 3);
         $data["command"]["duration"] = $duration;
-        // ----
-
-
-        $data["result"] = json_decode(implode("\n", $output), true);
-        ksort($data["result"]);
-        foreach ($data["result"]["streams"] as $i => $stream) {
-            $data["result"]["streams"][$i]["type"] = $stream["codec_type"];
-            if (
-                $data["result"]["format"]["nb_streams"] == 1
-                && $stream["codec_type"] == "video"
-                && $stream["avg_frame_rate"] == "0/0"
-                && in_array($stream["codec_name"], $this->image_formats)) {
-                $data["result"]["streams"][$i]["type"] = "image";
-            }
-            ksort($data["result"]["streams"][$i]);
-        }
-        if (isset($data["result"]["format"]) && isset($data["result"]["streams"][0])) {
-            ksort($data["result"]["format"]);
-        }
 
         return $data;
     }
---
+++ tests/DcpProbeTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Brightfish\MediaAnalyzer\Tests;
+
+use Brightfish\MediaAnalyzer\DcpProbe;
+use PHPUnit\Framework\TestCase;
+
+class DcpProbeTest extends TestCase
+{
+
+}
